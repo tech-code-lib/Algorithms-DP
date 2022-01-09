cabc3a26df6737adeef65e6ff6b294c137ebdc01
Nitin Rastogi <nitin.rastogi@live.in>
2022-01-08 19:51:47 -0500

    Added recursive call

--- HouseRobber/Program.cs
+++ HouseRobber/Program.cs
@@ -7,14 +7,42 @@ namespace HouseRobber
         static void Main(string[] args)
         {
             int[] nums = { 2, 7, 9, 3, 1};
+
+            //Recursive
+            //int result = RobRecursive(nums, nums.Length);
+
             //Call Rob with TC O(n), SC with o(n)
             //int result = Rob(nums);
 
             //Optimized Call Rob with TC O(n), SC with o(1)            
-            int result = Rob2(nums); 
+            int result = Rob2(nums);
+
             Console.WriteLine(result);
         }
 
+        private static int MaxEndsHere(int[] nums, int curr)
+        {
+            if (curr == 0)
+                return nums[0];
+            if (curr == 1)
+                return Math.Max(nums[0], nums[1]);
+            return Math.Max(MaxEndsHere(nums, curr - 1), (MaxEndsHere(nums, curr - 2) + nums[curr]));
+        }
+        public static int RobRecursive(int[] nums, int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return nums[0];
+            int max = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < n; i++)
+            {
+                max = Math.Max(max, MaxEndsHere(nums, i));
+            }
+
+            return max;
+        }
         /// <summary>
         /// Solution with DP and Time Complexity O(n) and Space Complexity also O(n)
         /// </summary>
