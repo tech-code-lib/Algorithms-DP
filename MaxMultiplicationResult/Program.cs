70b13e242ff2cde7d8dd5afdabe5459dc79c2808
Nitin Rastogi <nitin.rastogi@live.in>
2022-01-16 15:51:28 -0500

    MaxMultiplication Result and Minimum Difficulty

---
+++ MaxMultiplicationResult/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaxMultiplicationResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -5, -3, -3, -2, 7, 1 };
+            int[] mult = { -10, -5, 3, 4, 6 };
+            memo = new int[mult.Length, mult.Length];
+            Console.WriteLine(GetMax(nums, mult, 0, 0));
+        }
+
+        static int[,] memo;
+        static int GetMax(int[] nums, int[] mult, int i, int k)
+        {            
+            if (k == (mult.Length))
+                return 0;
+
+            int n = nums.Length;
+            int j = (n - 1) - (k - i);
+            if(memo[k, i] == 0)            
+            memo[k, i] = Math.Max(
+                (nums[i]*mult[k] + GetMax(nums, mult, i+1, k+1)),
+                (nums[j]*mult[k] + GetMax(nums, mult, i, k+1))
+                );
+
+            return memo[k, i];
+        }
+    }
+}
---
+++ MinimumDifficulty/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MinimumDifficulty
+{
+    //https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
+    class Program
+    {
+        /// <summary>
+        /// index
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumDifficulty(new int[]{ 6, 5, 4, 3, 2, 1 }, 3));
+        }
+
+        static int MinimumDifficulty(int[] jobDifficulty, int d)
+        {
+            if (d > jobDifficulty.Length)
+                return -1;
+
+            int[,] dp = new int[d+1, jobDifficulty.Length];
+            for (int i = 0; i < dp.GetLength(0); i++)
+            {
+                for (int j = 0; j < dp.GetLength(1); j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+            return dfs(jobDifficulty, d, 0, dp);
+        }
+
+        static int dfs(int[] diff, int d, int idx, int[, ] dp)
+        {
+            if (d == 1)
+            {
+                int max = 0;
+                while (idx < diff.Length)
+                {
+                    max = Math.Max(max, diff[idx]);
+                    idx++;
+                }
+                return max;
+            }
+            if (dp[d, idx] != -1)
+                return dp[d, idx];
+            int result = int.MaxValue;
+            int max_v = 0;
+            for (int i = idx; i < diff.Length - d + 1; i++)
+            {
+                max_v = Math.Max(max_v, diff[idx]);
+                result = Math.Min(result, max_v + dfs(diff, d - 1, i+1, dp)); 
+            }
+            dp[d, idx]=result;
+            return dp[d, idx];
+        }
+
+
+    }
+}
